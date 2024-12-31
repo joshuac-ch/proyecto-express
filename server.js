79a0df604d0cbd59181931ba5844724e710762a7
joshua <condorenajoshua98@gmail.com>
2024-12-31 14:49:45 -0500

    se mejoro la conexion en server.js

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ import express from "express"
 import path from "path"
 const app=express()
 const port=3000
-
+let users=[]
+app.get("/users",(req,res)=>{
+    res.json(users)
+})
 app.get("/",(req,res)=>{
     res.send("<h1>Hello word is the principal</h1>")
 })
-app.use(express.static('public'));
+
+//app.use(express.static('public'));
 app.listen(port,()=>{
     console.log(`http://localhost:${port}/`)
 })
